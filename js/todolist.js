9f6f931ac0fd830c317d09a918fa26bad947af66
prudhomme-dev <60824937+prudhomme-dev@users.noreply.github.com>
2022-01-28 17:41:41 +0100

    Modification du CSS / Ajout des fonctionnalités de suppression / de déclarer une tâche terminée

--- js/todolist.js
+++ js/todolist.js
@@ -1,93 +1,133 @@
 class task {
-    constructor(title, priority, status=false){
-        this.title=title
-        this.priority=priority
-        this.status=status
+    constructor(title, priority, status = false) {
+        this.title = title
+        this.priority = priority
+        this.status = status
     }
 }
-let tasks = []
-if (JSON.parse(localStorage.getItem("Task")))
-{
-    tasks = JSON.parse(localStorage.getItem("Task"))
+
+const saveStorage = (key, data) => {
+    let jsonTask = JSON.stringify(data)
+    localStorage.setItem(key, jsonTask)
 }
 
-console.log(tasks)
+const readStorage = (key) => {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+let tasks = []
+if (JSON.parse(localStorage.getItem("Task"))) {
+    tasks = readStorage("Task")
+}
 
 let priorityTask = document.querySelectorAll(".priorityTask")
 let list = document.querySelector(".list")
 let listArchives = document.querySelector(".listArchives")
 let choiceUser = 1
 
-const saveStorage = (key, data) =>
-{
-    let jsonTask=JSON.stringify(data)
-    localStorage.setItem(key, jsonTask)
-}
-
 const addTask = (e) => {
     if (e.key == "Enter" && e.target.value) {
         tasks.push(new task(document.querySelector("#newTask").value, parseInt(choiceUser)))
-        console.log(tasks)
         saveStorage("Task", tasks)
-        document.querySelector("#newTask").value=""
+        document.querySelector("#newTask").value = ""
         //console.log(JSON.parse(localStorage.getItem("Task")))
         displayTasks()
+    }
+}
+
+const deleteTask = (e) => {
+    tasks.splice(e.target.dataset.idTask, 1)
+    saveStorage("Task", tasks)
+    // Mettre en place le système de toast
+    displayTasks()
+
+}
+
+const finishTask = (e) => {
+    tasks[e.target.dataset.idTask].status = true
+    saveStorage("Task", tasks)
+    // Mettre en place le système de toast
+    displayTasks()
+}
+
+const editTask = (e) => {
+    let newInput = document.createElement("input")
+    let task = document.querySelectorAll(".task")
+    newInput.type="text"
+    newInput.value=e.target.innerText
+    newInput.addEventListener("keydown", editTaskValidation)
+    for (let i=0; i<task.length; ++i)
+    {
+        if (task[i].dataset.idTask == e.target.dataset.idTask) {
+            task[i].insertBefore(newInput, task[i].querySelector(".titleTask"))
+
+        }
+    }
+    e.target.style.display="none"
+}
+
+const editTaskValidation = (e) => {
+    if (e.key=="Escape")
+    {
+        console.log(e)
+        e.target.remove()
+        console.log(e.key, e.target.value)
     }
     
 }
 
-const deleteTask = (e) =>
-{
-    console.log("Suppression ", e.target.dataset.idTask)
-}
+const displayTasks = () => {
+    list.innerHTML = ""
+    listArchives.innerHTML = ""
+    for (let i = 0; i < tasks.length; ++i) {
+        let Newtask = document.createElement("div")
+        let newH3 = document.createElement("h3")
+        let newDivAction = document.createElement("div")
+        let newAFinish = document.createElement("a")
+        let newADelete = document.createElement("a")
+        newDivAction.classList.add("actionTask")
+        newDivAction.classList.add("flex")
+        Newtask.classList.add("task")
+        Newtask.dataset.idTask = i
+        Newtask.classList.add("flex")
+        newH3.classList.add("titleTask")
+        newH3.innerHTML = tasks[i].title
+        newH3.title = tasks[i].title
+        newH3.dataset.idTask = i
+        newH3.addEventListener("dblclick", editTask)
+        if (tasks[i].status == 0) {
 
-const finishTask = (e) =>{
-    console.log("Marquer comme terminé ", e.target.dataset.idTask)
-}
+            if (tasks[i].priority == 0) Newtask.classList.add("lowPriority")
+            else if (tasks[i].priority == 1) Newtask.classList.add("normalPriority")
+            else if (tasks[i].priority == 2) Newtask.classList.add("importantPriority")
 
-const displayTasks = () =>
-{
-    list.innerHTML=""
-    for (let i=0; i<tasks.length; ++i)
-    {
-        if (tasks[i].status==0)
-        {
-            let Newtask = document.createElement("div")
-            let newH3 = document.createElement("h3")
-            let newDivAction = document.createElement("div")
-            let newAFinish = document.createElement("a")
-            let newADelete = document.createElement("a")
-            newDivAction.classList.add("actionTask")
-            newDivAction.classList.add("flex")
-            Newtask.classList.add("task")
-            Newtask.classList.add("flex")
-            if (tasks[i].priority==0) Newtask.classList.add("lowPriority")
-            else if (tasks[i].priority==1) Newtask.classList.add("normalPriority")
-            else if (tasks[i].priority==2) Newtask.classList.add("importantPriority")
-            newH3.classList.add("titleTask")
-            newH3.innerHTML=tasks[i].title
             list.appendChild(Newtask)
-            Newtask.appendChild(newH3)
-            Newtask.appendChild(newDivAction)
-            newAFinish.dataset.idTask=i
-            newADelete.dataset.idTask=i
-            newADelete.addEventListener("click", deleteTask)
             newAFinish.addEventListener("click", finishTask)
+            newAFinish.dataset.idTask = i
             newAFinish.classList.add("finishTask")
-            newADelete.classList.add("deleteTask")
             newDivAction.appendChild(newAFinish)
-            newDivAction.appendChild(newADelete)
+        } else {
+            Newtask.classList.add("archived")
+            listArchives.appendChild(Newtask)
         }
+        newADelete.dataset.idTask = i
+        newADelete.addEventListener("click", deleteTask)
+        newADelete.classList.add("deleteTask")
+        newDivAction.appendChild(newADelete)
+        Newtask.appendChild(newH3)
+        Newtask.appendChild(newDivAction)
 
     }
+    if (document.querySelectorAll(".listArchives .task").length) document.querySelector(".taskArchivedTitle").style.display="block"
+    else document.querySelector(".taskArchivedTitle").style.display="none"
 }
 
 const clickPriority = (e) => {
     choiceUser = e.target.dataset.priority
     for (let i = 0; i < priorityTask.length; ++i) {
-        if (choiceUser==i) priorityTask[i].classList.add("selectPriority")
+        if (choiceUser == i) priorityTask[i].classList.add("selectPriority")
         else priorityTask[i].classList.remove("selectPriority")
-        
+
     }
 }
 
